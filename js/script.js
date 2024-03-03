735653fc6025a54ef85b00fc76dd0e293c632d47
Guilherme Rissi <102769917+glrmrissi@users.noreply.github.com>
2024-03-03 16:05:15 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,3 +13,30 @@ window.onscroll = function() {
     progress.style.height = progressHeight + "%"
 }
 
+// Seleciona todos os elementos de rádio com o nome "slider"
+let radioButtons = document.querySelectorAll('input[type="radio"][name="slider"]');
+
+// Adiciona um ouvinte de evento de mudança a cada botão de rádio
+radioButtons.forEach(function(button, index) {
+    button.addEventListener('change', function() {
+        // Itera sobre os botões de rádio para encontrar qual está marcado como selecionado
+        for (let i = 0; i < radioButtons.length; i++) {
+            if (radioButtons[i].checked) {
+                // Seleciona o elemento de informação correspondente ao botão de rádio atual
+                let info = document.querySelector('.info' + (i + 1));
+                
+                // Exibe o elemento de informação correspondente e oculta os outros
+                for (let j = 1; j <= radioButtons.length; j++) {
+                    let currentInfo = document.querySelector('.info' + j);
+                    currentInfo.style.display = (j === (i + 1)) ? 'inline' : 'none';
+                }
+                
+                // Se precisar de um log
+                console.log("O item " + (i + 1) + " está ativo.");
+                
+                // Sai do loop, pois encontramos o botão de rádio selecionado
+                break;
+            }
+        }
+    });
+});
